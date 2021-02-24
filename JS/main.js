e36fb8078a4b335a10cda35ab4c7b77df33d8448
andreluis98 <andreluis0017@gmail.com>
2021-02-24 16:38:44 -0300

    Arquivo Aula

---
+++ JS/main.js
@@ -0,0 +1,103 @@
+/*
+var nome = "People";
+var idade = 29;
+var frase = "Japan the best Selection the world"
+alert("Welcome " + nome );
+console.log(frase.replace("Japan","Brasil"));
+var idade2 = 10;
+alert(nome + "tem" + idade + "anos");
+alert(idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
+alert(frase.replace("Japan", "Brasil"));
+console.log(frase.toUpperCase); -> Coloca tudo em maiusculo;
+console.log(frase.toLowerCase); -> Coloca tudo em minusculo;
+*/
+
+/*  ARRAY
+var lista = ["maça", "pera", "laranja"];
+console.log(lista); -> IMPRIME(MOSTRA) A LISTA COMPLETA NO CONSOLE DO NAVEGADOR;
+
+var lista = ["maça", "pera", "laranja"];
+console.log(lista[1]); -> iMPRIME(MOSTRA) SÓ O ITEM "PERA" NESSA LISTA;
+alert(lista[1]);
+
+var lista = ["maça", "pera", "laranja"];
+lista.push("uva"); -> Adiciona mais um item a lista.
+console.log(lista);
+
+var lista = ["maça", "pera", "laranja"];
+lista.pop(); -> Tira o ultimo elemento da lista.
+console.log(lista);
+
+var lista = ["maça", "pera", "laranja"];
+console.log(lista.length); -> Mostra a quantidade de elementos que a lista tem.
+
+var lista = ["maça", "pera", "laranja"];
+console.log(lista.reverse()); - Mostra do ultimo ao primeiro item da lista.
+
+var lista = ["maça", "pera", "laranja"];
+console.log(lista);
+console.log(lista.toString); -> Transforma a lista de elementos para string.
+
+var lista = ["maça", "pera", "laranja"];
+console.log(lista);
+console.log(lista.join(" ")); - Separa a lista com o que tiver dentro das aspas.
+*/
+
+/*
+var lista = ["maça", "pera", "laranja"];
+lista.push("uva");
+lista.pop();
+console.log(lista);
+*/
+/* DICIONARIO
+var fruta = {nome: "maça", cor"vermelha"};
+console.log(fruta)
+
+var fruta = {nome: "maça", cor"vermelha"};
+console.log(fruta.nome);
+alert(fruta.cor);
+ */
+
+/* Lista de Dicionario
+var frutas = [{nome: "maça", cor"vermelha"}, {nome: "uva", cor"roxa"}];
+console.log(frutas);
+alert(frutas[1].nome); 
+*/
+/* LAÇOS DE REPETIÇÃO E CONDICIONAIS;
+var idade = prompt("Qual Sua idade?");
+//var idade = 18;
+if(idade >= 18){
+    alert(" Maior de idade");
+}else{
+    alert(" Menor de idade");
+}; 
+
+var count = 0; 
+while(count < 5){
+    console.log(count);
+    alert(count)
+    count++
+    count = count + 1;
+}
+
+var count;
+for(count=0; count <= 5; count++){
+    alert(count);
+};
+*/
+/* DATE
+var d = new Date();
+alert(d); -> Mostra a dat atual em ingles.
+
+var d = new Date();
+alert(d.getMonth()+1); -> Mostra o Mês atual, sempre precisa colocar mais 1 pq ele começã do 0.
+
+var d = new Date();
+alert(d.getMinutes()); -> Mostra os minutos.
+alert(d.getDay()); -> Mosta o Dia.
+alert(d.getHours()); -> Mostra a hora.
+*/
+
+
